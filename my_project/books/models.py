a1da5fec4b86a25d30af280b570ba8853ea5995d
BIGGODDOG <Gann.ia98@gmail.com>
2024-11-16 01:45:28 +0500

    task 3

--- my_project/books/models.py
+++ my_project/books/models.py
@@ -4,6 +4,7 @@ class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     published_date = models.DateField()
+    is_published = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
--- my_project/books/views.py
+++ my_project/books/views.py
@@ -4,7 +4,52 @@ from rest_framework import status
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, ViewSet
+
+class BookViewSet(ViewSet):
+    def list(self, request):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        try:
+            book = Book.objects.get(pk=pk)
+        except Book.DoesNotExist:
+            return Response({"error": "Book not found"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = BookSerializer(book)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        try:
+            book = Book.objects.get(pk=pk)
+        except Book.DoesNotExist:
+            return Response({"error": "Book not found"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = BookSerializer(book, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        try:
+            book = Book.objects.get(pk=pk)
+        except Book.DoesNotExist:
+            return Response({"error": "Book not found"}, status=status.HTTP_404_NOT_FOUND)
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def published(self, request):
+        books = Book.objects.filter(is_published=True)
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
 
 class BookAPIView(APIView):
     def get(self, request):
@@ -61,6 +106,6 @@ class BookDetailView(RetrieveUpdateDestroyAPIView):
     serializer_class = BookSerializer
 
 
-class BookViewSet(ModelViewSet):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+# class BookViewSet(ModelViewSet):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
